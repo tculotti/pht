9e1e48ef5c5bfc20d6c85b005f4157c883099128
Tom Culotti <tculotti@tculotti-mac.phtcorp.com>
2015-03-27 09:14:54 -0400

    added call to Jira CLI tools for user list

--- personXMLGen.py
+++ personXMLGen.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import subprocess
 from pprint import pprint
 
 
@@ -34,11 +35,18 @@ def gen_xml_file(jira_user_data, jama_user_data):
 if __name__ == "__main__":
 
     in_file_jama       = sys.argv[1]
-    in_file_jira       = sys.argv[2]
+    #in_file_jira       = sys.argv[2]
     
-    # Prepare Jira data
-    jira_user_data = [line.strip() for line in open(in_file_jira, 'r')]
+    # Prepare Jira data (input file method used before CLI)
+    #jira_user_data = [line.strip() for line in open(in_file_jira, 'r')]
     #print jira_user_data
+
+    jira_user_data = []
+    jira_get_user_list = subprocess.Popen(["../../work/jira-cli-4.1.0/jira.sh", "--action", "getUserList", "--group", "jira-users"], stdout=subprocess.PIPE).communicate()[0]
+    gen_list = jira_get_user_list.split("\n")
+    for line in gen_list[+2:-2]:
+        jira_user_data.append(line.split(",")[0].strip('"'))
+
     
     #Pepare Jama data from json file
     json_data=open(in_file_jama)
